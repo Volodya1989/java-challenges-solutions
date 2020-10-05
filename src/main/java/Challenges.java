8f910ba6244097d6895e6ad90b51ca8987f72c30
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-10-05 06:29:06 -0400

    Kids With the Greatest Number of Candies(Array)

--- src/main/java/Challenges.java
+++ src/main/java/Challenges.java
@@ -1,6 +1,34 @@
 package src.main.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Challenges {
+    public static void main(String[] args) {
+
+    }
+//    1431. Kids With the Greatest Number of Candies
+//    Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+//    For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them.
+//    Notice that multiple kids can have the greatest number of candies.
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean>candiesList = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for(int i:candies){
+            if(i>max)
+                max=i;
+            // if(int current(i)+extraCandies>)
+        }
+        for(int i: candies){
+            if((i+extraCandies)>=max){
+                candiesList.add(true);
+            }else{
+                candiesList.add(false);
+            }
+        }
+        return candiesList;
+    }
 //    Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 //    Return the running sum of nums.
     public int[] runningSum(int[] nums) {

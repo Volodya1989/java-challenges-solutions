2f88e3b9842b4baee0e8c349dfa4f556f50dd25b
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-10-07 11:47:04 -0400

    Remove Outermost Parentheses

--- src/main/java/Challenges.java
+++ src/main/java/Challenges.java
@@ -7,6 +7,27 @@ public class Challenges {
     public static void main(String[] args) {
 
     }
+//    1021. Remove Outermost Parentheses
+    //    A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+//    A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+//    Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+//    Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+    public String removeOuterParentheses(String S) {
+        int count = 0;
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder res =  new StringBuilder();
+
+        for(char ch: S.toCharArray()){
+            sb.append(ch);
+            count+=ch=='(' ? 1:-1;
+            if(count ==0){
+                res.append(sb.substring(1,sb.length()-1));
+                sb=new StringBuilder();
+            }
+        }
+        return res.toString();
+    }
 
     //1572. Matrix Diagonal Sum
 //    Given a square matrix mat, return the sum of the matrix diagonals.

291fb5c32c72e674d1955c3883ee85b20b834970
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-10-27 13:08:39 -0400

    max profit

---
+++ src/main/java/MaxProfit.java
@@ -0,0 +1,21 @@
+package src.main.java;
+
+public class MaxProfit {
+//    121. Best Time to Buy and Sell Stock
+//    Say you have an array for which the ith element is the price of a given stock on day i.
+//
+//    If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+//
+//    Note that you cannot sell a stock before you buy one.
+public int maxProfit(int[] prices) {
+    int maxprofit = 0;
+    for(int i=0; i<prices.length-1; i++){
+        for(int j=i+1; j<prices.length; j++){
+            int profit = prices[j]-prices[i];
+            if(profit>maxprofit)
+                maxprofit=profit;
+        }
+    }
+    return maxprofit;
+}
+}

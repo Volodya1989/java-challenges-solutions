39ab7a0c14207abf7647d2290648bd61d9a8ca33
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-11-13 10:30:59 -0500

    added Reorder Data in Log Files algorithm

--- src/main/java/OccurrenceGivenWordInGivenArray.java
+++ src/main/java/OccurrenceGivenWordInGivenArray.java
@@ -6,11 +6,6 @@ public class OccurrenceGivenWordInGivenArray {
 //    Find out the occurrence of a given word in a given array. The word may occur left to right, up to down and vice versa.
 
     public static void occurrenceGivenWordInGivenArray(String[] arr) {
-//        List asList = Arrays.asList(arr);
-//        Set<String> mySet = new HashSet<String>(asList);
-//        for (String s : mySet) {
-//            System.out.println(s + " " + Collections.frequency(asList, s));
-//        }
         List asList = Arrays.asList(arr);
         Set<String> mySet = new HashSet<String>(asList);
         for (String s : mySet) {
